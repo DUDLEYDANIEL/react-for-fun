f9577a53ca44b1a1586c8de72d3d51450277f77d
DUDLEYDANIEL <dudleydaniel.raj2005@gmail.com>
2024-08-05 19:00:59 +0530

    finished the CardPage.jsx

--- basics_react/src/Card.jsx
+++ basics_react/src/Card.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
 const Card = () => {
-   return( <>
-    <h1>Card</h1>
+   return( 
+     <>
+    <div className="card">
+    <h1 className="card-title">NEXUS PRIME</h1>
+    <img className="card-image" alt="this is a logo of nexus prime" src="https://imgs.search.brave.com/irM3iC0n6dwtWJKiYs_M614vnJ2wAveUdhZqMDsAKqE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL1Mv/YXBsdXMtbWVkaWEt/bGlicmFyeS1zZXJ2/aWNlLW1lZGlhLzEx/MjA1Y2VkLWViZmYt/NGM0Zi1iZjI4LTYx/NjZkODc3ZGFjNi5f/X0NSMSwwLDcyMiw5/MDRfUFQwX1NYMzYy/X1YxX19fLmpwZw"></img>
+    <p className="card-text">we are community to improve the developing community</p>
+    </div>
     </>);
 }
 
--- basics_react/src/main.jsx
+++ basics_react/src/main.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-//import App from './App.jsx'
-import Card from './Card.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import Card from './Card.jsx';
+import './index.css';
+import CardPage from './Card-page.jsx';
+import Button from './Buttons.jsx';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <div>Oops! There was an error.</div>,
+  },
+  {
+    path: '/card',
+    element: <CardPage />,
+  },{
+    path:'/button',
+    element:<Button/>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Card />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
